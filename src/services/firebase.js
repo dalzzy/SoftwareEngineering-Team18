ea66af2f3998fe3da6afc8e7301e5ca156d73242
JIUUOO <nninjiuuoo@gmail.com>
2025-05-28 02:09:30 +0900

    feat: firestore 초기화

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -17,5 +18,8 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 export default app;
-export { auth };
+export { auth, db };
